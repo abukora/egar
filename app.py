0a915ad9186bf256f0aea2323cf74071345e6e48
abukora <abukora8@gmail.com>
2024-11-29 00:05:03 +0300

    Update app.py
    Signed-off-by: abukora <abukora8@gmail.com>

--- app.py
+++ app.py
@@ -14,33 +14,37 @@ def get_db_connection():
 def index():
     return render_template('index.html')
 
-# دالة لإضافة المستأجر إلى قاعدة البيانات
+# دالة لإضافة مستأجر جديد
 @app.route('/add_tenant', methods=['POST'])
 def add_tenant():
-    if request.method == 'POST':
-        tenant_name = request.form['tenant_name']
-        land_paper = request.form['land_paper']
-        plot_number = request.form['plot_number']
-        feddan = int(request.form['feddan'])
-        qirat = int(request.form['qirat'])
-        share = int(request.form['share'])
-        violations = request.form['violations']
-        
-        # الاتصال بقاعدة البيانات
-        conn = get_db_connection()
-        c = conn.cursor()
+    tenant_name = request.form['tenant_name']
+    land_paper = request.form['land_paper']
+    plot_number = request.form['plot_number']
+    feddan = int(request.form['feddan'])
+    qirat = int(request.form['qirat'])
+    share = int(request.form['share'])
+    violations = request.form['violations']
+    
+    # الاتصال بقاعدة البيانات
+    conn = get_db_connection()
+    conn.execute('''
+        INSERT INTO tenants (tenant_name, land_paper, plot_number, feddan, qirat, share, violations)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+    ''', (tenant_name, land_paper, plot_number, feddan, qirat, share, violations))
+    
+    # حفظ التغييرات
+    conn.commit()
+    conn.close()
 
-        # إضافة بيانات المستأجر إلى الجدول
-        c.execute('''
-            INSERT INTO tenants (tenant_name, land_paper, plot_number, feddan, qirat, share, violations)
-            VALUES (?, ?, ?, ?, ?, ?, ?)
-        ''', (tenant_name, land_paper, plot_number, feddan, qirat, share, violations))
-        
-        # حفظ التغييرات
-        conn.commit()
-        conn.close()
+    return redirect(url_for('tenants'))
 
-        return redirect(url_for('index'))
+# صفحة عرض جميع المستأجرين
+@app.route('/tenants')
+def tenants():
+    conn = get_db_connection()
+    tenants = conn.execute('SELECT * FROM tenants').fetchall()
+    conn.close()
+    return render_template('tenants.html', tenants=tenants)
 
 # تشغيل التطبيق
 if __name__ == '__main__':
